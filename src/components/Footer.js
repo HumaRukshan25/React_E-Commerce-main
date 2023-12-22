b3ef434d3bb052fce36a19d9bcbfc2e77c095cc0
HumaRukshan25 <1hk19cs061@hkbk.edu.in>
2023-12-22 17:50:16 +0530

    completed added icons ansd style

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,14 +3,30 @@ import React from "react";
 const Footer = () => {
   return (
     <>
+      <style>
+        {`
+          .social-icon {
+            margin-right: 10px;
+          }
+        `}
+      </style>
       <footer className="mb-0 text-center">
         <div className="d-flex align-items-center justify-content-center pb-5">
           <div className="col-md-6">
-            <p className="mb-3 mb-md-0">Made with by {" "}
-              <a  href="https://HumaRukshan.dev" className="text-decoration-underline text-dark fs-5" target="_blank" rel="noreferrer">Huma Rukshan</a>
+            <p className="mb-3 mb-md-0">Made by{" "}
+              <a href="https://HumaRukshan.dev" className="text-decoration-underline text-dark fs-5" target="_blank" rel="noreferrer">Huma Rukshan</a>
             </p>
-            <a className="text-dark fs-4" href="https://github.com/HumaRukshan25" target="_blank" rel="noreferrer">
+            <a className="text-dark fs-4 social-icon" href="https://github.com/HumaRukshan25" target="_blank" rel="noreferrer">
               <i className="fa fa-github"></i>
+            </a>
+            <a className="text-dark fs-4 social-icon" href="https://www.youtube.com" target="_blank" rel="noreferrer">
+              <i className="fab fa-youtube"></i>
+            </a>
+            <a className="text-dark fs-4 social-icon" href="https://open.spotify.com" target="_blank" rel="noreferrer">
+              <i className="fab fa-spotify"></i>
+            </a>
+            <a className="text-dark fs-4 social-icon" href="https://www.facebook.com" target="_blank" rel="noreferrer">
+              <i className="fab fa-facebook"></i>
             </a>
           </div>
         </div>
